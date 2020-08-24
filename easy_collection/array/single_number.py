583766779660698ad5186b6beb53d149848d0346
Luke Yeh <lukeoverrhere@gmail.com>
2020-08-24 15:10:48 -0400

    single_number.py

---
+++ easy_collection/array/single_number.py
@@ -0,0 +1,52 @@
+from typing import List
+
+# Question
+# --------------
+# Single Number
+# --------------
+#
+# Link
+# --------------
+# https://leetcode.com/problems/contains-duplicate/
+# --------------
+#
+# Description
+# --------------
+# Given a non-empty array of integers, every element appears twice except for
+# one. Find that single one.
+#
+# Note:
+# Your algorithm should have a linear runtime complexity. Could you implement
+# it without using extra memory?
+# --------------
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # One-liner:
+        # Time: O(n)
+        # Space: O(n)
+        # Counter(nums).most_common()[-1][0]
+        b = 0
+        for num in nums:
+            b ^= num
+
+        return b
+
+
+def test1():
+    solution = Solution()
+    nums = [1, 2, 3, 4]
+    assert not solution.containsDuplicate(nums)
+
+
+def test2():
+    solution = Solution()
+    nums = [1, 1, 2, 3]
+    assert solution.containsDuplicate(nums)
+
+
+def test3():
+    solution = Solution()
+    nums = [1, 2, 2, 3]
+    assert solution.containsDuplicate(nums)
